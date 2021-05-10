532e0e41af74a2f40ab6eaadac0d794730e76844
ssm29njit <71558799+ssm29njit@users.noreply.github.com>
2021-05-10 18:42:51 -0400

    Update email_token.py

--- app/server/mail/email_token.py
+++ app/server/mail/email_token.py
@@ -8,14 +8,3 @@ def generate_confirmation_token(email):
     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
 
 
-def confirm_token(token, expiration=3600):
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-    try:
-        email = serializer.loads(
-            token,
-            salt=app.config['SECURITY_PASSWORD_SALT'],
-            max_age=expiration
-        )
-    except:
-        return False
-    return email
